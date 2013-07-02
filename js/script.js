7cf6b2fdfc01221dbf10ef0577e89d024422305b
Hack Reactor Students <github@hackreactor.com>
2013-07-02 16:56:20 -0700

    working on grabbing by date

--- js/script.js
+++ js/script.js
@@ -1,25 +1,54 @@
 (function() {
+  var app = {};
+  app.parseUrl = 'https://api.parse.com/1/classes/messages';
+  app.timestamp = '1900-01-21T01:01:00.000Z';
+
   var template = function(username, date, message) {
-    return '<div>' +
+    var html = '<div class="box">' +
       '<a class="username" href="#">' + username + '</a>' +
       '<span class="date">' + date + '</span>' +
       '<div class="message">' + message + '</div>' +
     '</div>';
+    return malString(html) ? '' : html;
   };
 
-  $.ajax({
-    url: 'https://api.parse.com/1/classes/messages',
-    contentType: 'application/json',
-    success: function(data){
-      for(var i = 0; i < results.length; i++) {
-        var username = results[i].username;
-        var date = results[i].createdAt;
-        var message = results[i].text;
-        //$('#main').append(template(results[0], results[0].createdAt, results[0].text));
+  var malString = function(string) {
+    return (/<script>/).test(string);
+  };
+
+  var dataString = 'where={"createdAt":{"$gt":{"__type":"Date","iso":"' + app.timestamp + '"}}}&limit=20&order=-createdAt';
+
+  var update = function() {
+    $('#main').html('');
+    $.ajax({
+      url: app.parseUrl,
+      data: dataString,
+      contentType: 'application/json',
+      success: function(data){
+        var result = data.results;
+        if(result[0].date > app.timestamp) {
+          app.timestamp = result[0].date;
+        }
+
+        for(var i = 0; i < result.length; i++) {
+          var username = '@' + result[i].username;
+          var date = moment().startOf(result[i].createdAt).fromNow();
+          var message = result[i].text;
+          $('#main').append(template(username, date, message));
+        }
+      },
+      error: function(data) {
+        console.log('Ajax request failed');
       }
-    },
-    error: function(data) {
-      console.log('Ajax request failed');
-    }
-  });
+    });
+    setTimeout(function() {
+      update();
+    }, 5000);
+  };
+
+  var init = function() {
+    update();
+  };
+  init();
+
 }());
