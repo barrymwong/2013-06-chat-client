f3281ea6e2cc1957bdd0fa16c1fb5eca1294903f
Hack Reactor Students <github@hackreactor.com>
2013-07-02 20:02:46 -0700

    adding changes

--- js/script.js
+++ js/script.js
@@ -3,22 +3,9 @@
   app.parseUrl = 'https://api.parse.com/1/classes/messages';
   app.timestamp = '1900-01-21T01:01:00.000Z';
 
-  var template = function(username, date, message) {
-    var html = '<div class="box">' +
-      '<a class="username" href="#">' + username + '</a>' +
-      '<span class="date">' + date + '</span>' +
-      '<div class="message">' + message + '</div>' +
-    '</div>';
-    return malString(html) ? '' : html;
-  };
-
-  var malString = function(string) {
-    return (/<script>/).test(string);
-  };
-
   var dataString = 'where={"createdAt":{"$gt":{"__type":"Date","iso":"' + app.timestamp + '"}}}&limit=20&order=-createdAt';
 
-  var update = function() {
+  var pullData = function() {
     $('#main').html('');
     $.ajax({
       url: app.parseUrl,
@@ -26,15 +13,15 @@
       contentType: 'application/json',
       success: function(data){
         var result = data.results;
-        if(result[0].date > app.timestamp) {
-          app.timestamp = result[0].date;
-        }
+        app.timestamp = result[0].createdAt;
+        // console.log(data);
 
         for(var i = 0; i < result.length; i++) {
-          var username = '@' + result[i].username;
-          var date = moment().startOf(result[i].createdAt).fromNow();
-          var message = result[i].text;
-          $('#main').append(template(username, date, message));
+          var username = $('<a/>').addClass('username').attr('href', '#').text('@' + result[i].username);
+          var date = $('<span/>').addClass('date').text(moment().startOf(result[i].createdAt).fromNow());
+          var message = $('<div/>').addClass('message').text(result[i].text);
+          var template = $('<div/>').addClass('box').append(username).append(date).append(message);
+          $('#main').append(template);
         }
       },
       error: function(data) {
@@ -42,13 +29,47 @@
       }
     });
     setTimeout(function() {
-      update();
+      pullData();
     }, 5000);
   };
 
+  var getUsername = function() {
+    var loc = (location.href).split('username=');
+    $('#username').text(loc[1]);
+    return loc[1];
+  };
+
+  var sendData = {
+    username: getUsername(),
+    text: $('#input-message').val()
+  };
+
+  var pushData = function() {
+    $.ajax({
+      url: app.parseUrl,
+      data: sendData,
+      contentType: 'application/json',
+      success: function(data){
+        console.log('sent!');
+      },
+      error: function(data) {
+        console.log('Ajax request failed');
+      }
+    });
+    $('#input-message').val(null).focus();
+  };
+
   var init = function() {
-    update();
+    pullData();
+    getUsername();
+    $('#form-message').on('submit', function(event) {
+      event.preventDefault();
+      pushData();
+    });
   };
   init();
 
 }());
+
+// if time is newer than last updated time, then request those.
+// remove the number of nodes at the bottom equal to the number of new requested
